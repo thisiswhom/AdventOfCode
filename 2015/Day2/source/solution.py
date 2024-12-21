e57a646c5df7cda6c91ecc68787e9ad139602805
Kevin Carter <kevin.r.carter.mil@army.mil>
2024-12-21 15:30:07 -0600

    present intake working, now onto converting the strings

--- 2015/Day2/source/solution.py
+++ 2015/Day2/source/solution.py
@@ -2,6 +2,10 @@
 
 class Rectangular_Prism_Present():
     def __init__(self, length, width, height):
+        """Takes the three different edges and calculates the three sets of rectangles
+
+        Having it do calculations in the __init__ may break convention however it made the
+        creation and use of the methods simpler"""
         self.length = length
         self.width = width
         self.height = height
@@ -10,5 +14,26 @@ class Rectangular_Prism_Present():
         self.hxw = height * width
 
     def surface_area(self):
+        """Uses the three equal and opposite faces and returns the surface area"""
         total_area = (2 * self.lxw) + (2 * self.lxh) + (2 * self.hxw)
         return total_area
+
+    def smallest_side(self):
+        """Uses the three equal and opposite faces and returns the smallest"""
+        small_side = min(self.lxw, self.lxh, self.hxw)
+        return small_side
+
+    def wrapping_paper_needed(self):
+        """Uses the previously calculated values to return the amount of wrapping paper needed"""
+        return self.surface_area() + self.smallest_side()
+
+
+
+def intake_presents(file):
+    """Opens a file then makes each line a string in list."""
+    input_file = open(f"{file}")
+    list_of_presents = input_file.readlines()
+    return list_of_presents
+
+def convert_present_string_to_ints(string):
+    pass
--- 2015/Day2/test/test.py
+++ 2015/Day2/test/test.py
@@ -6,6 +6,16 @@ import source.solution as solution
 def testable_rpp():
     return solution.Rectangular_Prism_Present(2,3,4)
 
+@pytest.fixture
+def sample_string():
+    return [2,3,4]
+
+mock_file_data = """2x3x4
+1x2x3
+"""
+
+
+
 def test_RPP_attributes(testable_rpp):
         #rpp = solution.Rectangular_Prism_Present()
         assert hasattr(testable_rpp, "length")
@@ -18,5 +28,21 @@ def test_RPP_attributes(testable_rpp):
 def test_RPP_surface_area(testable_rpp):
     assert testable_rpp.surface_area() == ((2*3)*2) + ((2*4)*2) + ((4*3)*2)
 
-def test_RPP_smallest_side(testatble_rpp):
-    pass
+def test_RPP_smallest_side(testable_rpp):
+    assert testable_rpp.smallest_side() == 6
+
+def test_wrapping_paper_needed(testable_rpp):
+    assert testable_rpp.wrapping_paper_needed() == 58
+
+
+def test_intake_presents(mocker):
+    """Simulates opening a file using the pytest-mock plugin
+
+    It's really cool how it says 'for this test, when you see the open fucntion
+     called, intercept it and instead return this other file"""
+    mocked_open = mocker.patch("builtins.open", mocker.mock_open(read_data=mock_file_data))
+    result = solution.intake_presents("dummy_file.txt")
+    assert result ==mock_file_data.splitlines(keepends=True)
+
+def test_convert_present_string_to_ints():
+    assert solution.convert_present_string_to_ints(sample_string) == [2,3,4]
